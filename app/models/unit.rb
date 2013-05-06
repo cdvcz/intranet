32fbffd4836f8172cd9a077f9d691e9981e1e306
David Štancl <dstancl@dstancl.cz>
2013-05-06 15:58:47 +0200

    Added: validation

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,5 +1,8 @@
 class Unit < ActiveRecord::Base
   attr_protected :created_at, :updated_at, :lft, :rgt
 
+  validates :name, presence: true
+  validates :abbreviation, presence: true
+
   acts_as_nested_set
 end
