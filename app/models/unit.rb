8e58788e16119776fa92820d97c98b2970c9a8a6
David Štancl <dstancl@dstancl.cz>
2013-05-07 12:29:02 +0200

    Cannot destroy unit with child

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -5,4 +5,21 @@ class Unit < ActiveRecord::Base
   validates :abbreviation, presence: true
 
   acts_as_nested_set
+
+  before_destroy :check_for_empty
+
+  def read_attribute_for_validation(attr)
+    attr
+  end
+
+  private
+
+  # Return false when has child and put error message
+  # 
+  def check_for_empty
+    if not self.leaf?
+      errors.add(:unit, :not_empty)
+      return false
+    end
+  end
 end
