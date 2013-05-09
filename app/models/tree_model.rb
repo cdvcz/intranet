3245a19b6a2745c4435a35597c0edcdd12281c49
David Štancl <dstancl@dstancl.cz>
2013-05-09 11:42:14 +0200

    Created tree model
    Tree model is common ancestor for models representing tree structure.
    There is defined method for JSON output as tree. See BaseTreeController.

---
+++ app/models/tree_model.rb
@@ -0,0 +1,15 @@
+class TreeModel < ActiveRecord::Base
+  self.abstract_class = true
+  attr_protected :lft, :rgt
+  acts_as_nested_set
+
+
+  # Helper for to_json: add children
+  #
+  # Adds children data to attribute "children"
+  def to_node
+    self.attributes.merge({:children => self.descendants.map {|c| c.to_node}})
+  end
+
+
+end
