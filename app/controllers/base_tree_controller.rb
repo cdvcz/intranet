d222658e012e4a1650eeb244c0c1819000579906
David Štancl <dstancl@dstancl.cz>
2013-05-09 15:03:39 +0200

    Bug-fix: use self for tree output
    For action "tree" was used old code from unit controller. Rewritten and used method "roots" for getting array of root nodes.

--- app/controllers/base_tree_controller.rb
+++ app/controllers/base_tree_controller.rb
@@ -1,9 +1,8 @@
 class BaseTreeController < BaseController
   # Render as tree structure
   def tree
-    @roots = Unit.find_all_by_parent_id(nil)
     respond_to do |format|
-      format.json { render :json => { :content => @roots.map{ |u| u.to_node } } }
+      format.json { render :json => { :content => self.roots.map{ |u| u.to_node } } }
     end
   end
 end
